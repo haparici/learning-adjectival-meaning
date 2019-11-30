2a8daca4154ab09396ca8148453932de19ed5c30
zhang-lucy <zhangly@mit.edu>
2019-11-30 14:16:34 -0500

    added a few comments

--- test_generation.py
+++ test_generation.py
@@ -6,6 +6,7 @@ if __name__=="__main__":
 	# For Quiz 1
 	# Test Examples
 	folder = "Q1"
+	#will they hypothesize on specific colors or radius distance?
 	stimulus1 = Stimulus(.05,.35,"y","b","Not pelty")
 	stimulus2 = Stimulus(.20,.25,"b","g","Pelty")
 	stimulus1.show(save=True,folder=folder)
@@ -23,6 +24,7 @@ if __name__=="__main__":
 	# For Quiz 2
 	# Test Examples
 	folder = "Q2"
+	#will they pick the equal colors or the radius distance?
 	stimulus1 = Stimulus(.1,.3,"r","r","Pelty")
 	stimulus1.show(save=True,folder=folder)
 
@@ -38,6 +40,7 @@ if __name__=="__main__":
 	# For Quiz 3
 	# Test Examples
 	folder = "Q3"
+	#possibility of compound hypothesis since s1 and s2 so different
 	stimulus1 = Stimulus(.05,.35,"y","y","Pelty")
 	stimulus2 = Stimulus(.20,.25,"b","g","Pelty")
 	stimulus3 = Stimulus(.15,.35,"r","k","Not pelty")
